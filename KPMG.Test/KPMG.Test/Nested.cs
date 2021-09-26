fbb04b3aa71f159d16f9f3c4d561c574d4d48636
Rahul Nehare <rahuln2822@gmail.com>
2021-09-26 13:45:28 +0530

    added nested object program

---
+++ KPMG.Test/KPMG.Test/Nested.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace KPMG_Test
+{
+    public class Nested
+    {
+        private string _key;
+        private string _value;
+        private Nested _self;
+
+        public Nested(string key, Nested self)
+        {
+            _key = key;
+            _self = self;
+        }
+
+        public Nested(string key, string value)
+        {
+            _key = key;
+            _value = value;
+        }
+
+        public List<string> GetKeys()
+        {
+            var keys = new List<string>();
+            keys.Add(_key);
+
+            Nested nestedObj = _self;
+
+            while (nestedObj != null) 
+            {
+                keys.Add(nestedObj._key);
+                nestedObj = nestedObj?._self;
+            };
+            
+            return keys;
+        }
+
+        public string GetValue()
+        {           
+            Nested nestedObj = _self;
+
+            if (nestedObj == null)
+                return _value;
+
+            while (nestedObj._value == null) 
+            {    
+                nestedObj = nestedObj._self;
+            };
+
+            return nestedObj?._value;
+        }
+
+        public object GetValueByKey(string key)
+        {
+            Nested nestedObj = _self;
+
+            if (nestedObj == null)
+                return _value;
+
+            while (nestedObj._value == null)
+            {
+                if (nestedObj._key == key)
+                {
+                    return nestedObj;
+                }
+                nestedObj = nestedObj._self;
+            };
+
+            return nestedObj?._value;
+        }
+    }
+}
---
+++ KPMG.Test/KPMG.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KPMG_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var nestedObj = new Nested("x", new Nested("y", new Nested("z", "a")));            
+            Console.WriteLine(string.Join("/", nestedObj.GetKeys()));
+            Console.WriteLine(nestedObj.GetValue());
+            Console.WriteLine(nestedObj.GetValueByKey("x"));
+            Console.ReadLine();
+        }
+    }
+}
